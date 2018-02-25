6b578316f1aaa0df549684360662fb1b5659f32d
sbohmann <mail@sebastian-bohmann.com>
2018-02-25 08:43:28 +0100

    Initial Commit

---
+++ SwiftPersistentCollections/SwiftPersistentCollections.h
@@ -0,0 +1,13 @@
+//
+//  SwiftPersistentCollections.h
+//  SwiftPersistentCollections
+//
+//  Created by Sebastian Bohmann on 25/02/2018.
+//  Copyright © 2018 Sebastian Bohmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwiftPersistentCollections : NSObject
+
+@end
---
+++ SwiftPersistentCollections/SwiftPersistentCollections.m
@@ -0,0 +1,13 @@
+//
+//  SwiftPersistentCollections.m
+//  SwiftPersistentCollections
+//
+//  Created by Sebastian Bohmann on 25/02/2018.
+//  Copyright © 2018 Sebastian Bohmann. All rights reserved.
+//
+
+#import "SwiftPersistentCollections.h"
+
+@implementation SwiftPersistentCollections
+
+@end
